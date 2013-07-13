f438ce58fbf04d94bec2b20c6613fdf66f3c4dbd
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-07-13 18:04:27 +0900

    Change the plot theme grey to bw
    The bw theme is better than the grey theme,
    when the plotted graph is pasted to keynote slide

--- src/plotter.R
+++ src/plotter.R
@@ -19,7 +19,7 @@ list2dataframe <- function(list){
 #function for plotting, output to pdf file
 ggplotter <- function(dataframe, pdf_path = "./", lwd = 1.0){
   g <- ggplot(dataframe, aes(x = volume, y = intensity, group = name, col = name))
-  default_plot <- g + geom_line(size = lwd) + theme_grey(22)
+  default_plot <- g + geom_line(size = lwd) + theme_bw(22)
   final_plot <- default_plot
   
   #generate Plot File Name from System Time
